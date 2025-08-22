054707ea293c208ccfaa63b336691af70886d2dd
Dedi Hartono <dedi.hartono@mad.co.id>
2025-08-22 16:15:26 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const token = process.env.TOKEN
+
+app.get('/webhooks',  (req, res) => {
+ if (
+   req.query['hub.mode'] == 'subscribe' &&
+   req.query['hub.verify_token'] == token
+ ) {
+   res.send(req.query['hub.challenge']);
+ } else {
+   res.sendStatus(400);
+ }
+})
+module.exports.handler = serverless(app);
